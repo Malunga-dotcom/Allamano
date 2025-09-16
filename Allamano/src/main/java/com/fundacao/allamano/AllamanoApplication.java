8cc3961c63acebab16ecdae869d9b7a9bc4c1784
Malunga-dotcom <116507891+Malunga-dotcom@users.noreply.github.com>
2025-09-16 23:50:00 +0100

    Add files via upload

---
+++ Allamano/src/main/java/com/fundacao/allamano/AllamanoApplication.java
@@ -0,0 +1,13 @@
+package com.fundacao.allamano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllamanoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllamanoApplication.class, args);
+    }
+
+}
---
+++ Allamano/src/test/java/com/fundacao/allamano/AllamanoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundacao.allamano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllamanoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
